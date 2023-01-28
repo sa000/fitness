c4cf55c62c2dbe34159d67db74c2a44fcf2ac638
sakib <alamsakib@gmail.com>
2023-01-28 16:07:20 +0000

    added dynmao db

--- runner.py
+++ runner.py
@@ -7,15 +7,7 @@ from helpers.video_utils import (
 )
 from model_generator import predict_on_unseen_data
 
-excercise = "hammer_curls"
-# os.system(f"python3 initialize_resources.py {excercise}")
-os.system(f"python3 image_aug.py {excercise}")
-os.system(f"python3 feature_generation.py {excercise}")
+excercise = "squat"
+
 os.system(f"python3 model_generator.py {excercise}")
 
-video_file = "hammer_curls_final.mp4"
-load_video_into_cv2(excercise, video_file)
-predict_on_unseen_data(excercise,video_file)
-generated_graded_video(excercise,video_file)
-convert_frames_to_gif(excercise,video_file)
- 
